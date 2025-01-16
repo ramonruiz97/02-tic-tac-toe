2f6f2a3cf6b8240bb9ec0c9822ad0834c5971845
Ramon Ruiz <ramonruiz@MacBook-Air-de-Ramon.local>
2025-01-16 18:42:16 +0100

    small

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,7 +2,6 @@ export default function Square ({children, isSelected, updateBoard, index}) {
   //Basandonos en el estado del padre (turn), 
   //cambiamos la visualizacion del estado hijo
   const className = `square ${isSelected ? 'is-selected' : ''}`
-  console.log(isSelected)
 
   const handleClick = () => {
     updateBoard(index)
